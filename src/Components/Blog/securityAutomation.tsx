94084486347a4f499c467da39aeee0a5b2ee3903
Maham-Mehar <mahammehar47@gmail.com>
2025-04-30 14:23:33 +0500

    push

--- src/Components/Blog/securityAutomation.tsx
+++ src/Components/Blog/securityAutomation.tsx
@@ -2,7 +2,7 @@ const SecurityAutomation = () => {
     return (
         <div className="bg-[url('/images/image5.png')] h-90 relative bg-center bg-cover items-center bg-no-repeat flex">
             <div className="absolute w-full h-90 bg-[#26324dD9]"></div>
-            <div className="relative sm:max-w-6xl max-w-4xl w-full mx-auto items-center px-3">
+            <div className="relative sm:max-w-6xl max-w-4xl w-full mx-auto items-center px-10">
                 <p className="text-lg font-semibold text-[#C0C4CE]">Perspective</p>
                 <p className="text-[30px] sm:text-[36px] xl:text-[42px] font-semibold text-white max-w-lg">
                     Thoughts on Security Automation</p>
