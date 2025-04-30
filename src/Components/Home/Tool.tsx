33d77f09847a136e9a3d42c473a35106de1f64e7
Maham-Mehar <mahammehar47@gmail.com>
2025-04-30 14:04:17 +0500

    push

--- src/Components/Home/Tool.tsx
+++ src/Components/Home/Tool.tsx
@@ -5,8 +5,8 @@ const Tools = () => {
       <div className="xl:px-25 lg:px-25 md:px-13 sm:px-10 px-8 py-16 xl:py-32 lg:py-27 md:py-23 sm:py-19 max-w-5xl mx-auto">
         {toolsData.map((item, index) => (
           <div key={index} className=" max-w-[700px]">
-            <p className="text-lg font-semibold text-[#1898D8]">{item.text1}</p>
-            <p className="text-[28px] xl:text-[42px] lg:text-[42px] md:text[38px] sm-text[32px] font-semibold leading-tight text-[#235380]">{item.Heading1}</p>
+            <p className="sm:text-lg text-md font-semibold text-[#1898D8]">{item.text1}</p>
+            <p className="text-[24px] xl:text-[42px] lg:text-[42px] md:text[38px] sm-text[32px] font-semibold leading-tight text-[#235380]">{item.Heading1}</p>
             <p className="text-base font-normal text-[#7F7177] mt-6">{item.text2}</p>
             <p className="text-base font-normal text-[#7F7177] mt-3">{item.text3}</p>
             <div className="xl:text-2xl md:text-[20px] text-[20px] ms-0 xl:ms-5 lg:ms-5 md:ms-5 sm:ms-5 font-semibold text-[#235380] ">
